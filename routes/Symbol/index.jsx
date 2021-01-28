fc332dc144240286e9984006f02880f624470462
karun kalantabutra <pondkarun.k@gmail.com>
2021-01-29 00:54:23 +0700

    binance

--- routes/Symbol/index.jsx
+++ routes/Symbol/index.jsx
@@ -1,273 +1,149 @@
-
 import { useState } from "react";
-import { Skeleton } from "@material-ui/lab";
-
+import { makeStyles } from "@material-ui/core/styles";
 import {
-  Grid,
-  Button,
-  Input,
-  InputLabel,
-  InputAdornment,
-  FormControl,
+  Paper,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow,
-  Paper,
-  TextField,
-  MenuItem,
 } from "@material-ui/core";
 
 const symbol = () => {
-  const [modelSave, setModelSave] = useState({
-    type: 1,
-    amount: 10000,
-    number: 50,
-    rate_start: 8,
-    rate_end: 9.5,
-    profit: 2.06,
-  });
-
-  const [rows, setRows] = useState([]);
-  const [sumProfit, setSumProfit] = useState(0);
-  const [currencies, setCurrencies] = useState([
+  const columns = [
+    { id: "name", label: "Name", minWidth: 170 },
+    { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
     {
-      value: 1,
-      label: "Basic",
+      id: "population",
+      label: "Population",
+      minWidth: 170,
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
     },
-  ]);
+    {
+      id: "size",
+      label: "Size\u00a0(km\u00b2)",
+      minWidth: 170,
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "density",
+      label: "Density",
+      minWidth: 170,
+      align: "right",
+      format: (value) => value.toFixed(2),
+    },
+  ];
 
-  const calculate = () => {
-    // console.log('modelSave :>> ', modelSave);
-    const tempDate = [];
-    let sum = 0;
-    if (modelSave.type === 1) {
-      const vat = 0.25 / 100; //ค่าธรรมเนียม 0.25%
-      const rate =
-        (modelSave.rate_end - modelSave.rate_start) / modelSave.number; //เรทที่ลดลง แต่ละไม้
-      const cost = Number((modelSave.amount / modelSave.number).toFixed(2)); //ทุน
-      const vatCost = Number((cost * vat).toFixed(2)); //ค่าธรรมเนียม 0.25% ซื้อ
+  function createData(name, code, population, size) {
+    const density = population / size;
+    return { name, code, population, size, density };
+  }
 
-      for (let x = 0; x < modelSave.number; x++) {
-        const buyPrice = Number(
-          (x === 0
-            ? modelSave.rate_end - rate
-            : tempDate[x - 1].buyPrice - rate
-          ).toFixed(2)
-        ); //คำนวณ ชื้อราคา
-        const amount = Number(((cost - vatCost) / buyPrice).toFixed(8)); //คำนวณ จำนวน
-        const sellPrice = Number(
-          (buyPrice + (buyPrice * modelSave.profit) / 100).toFixed(2)
-        ); //คำนวณ ราคาขาย
+  const rows = [
+    createData("India", "IN", 1324171354, 3287263),
+    createData("China", "CN", 1403500365, 9596961),
+    createData("Italy", "IT", 60483973, 301340),
+    createData("United States", "US", 327167434, 9833520),
+    createData("Canada", "CA", 37602103, 9984670),
+    createData("Australia", "AU", 25475400, 7692024),
+    createData("Germany", "DE", 83019200, 357578),
+    createData("Ireland", "IE", 4857000, 70273),
+    createData("Mexico", "MX", 126577691, 1972550),
+    createData("Japan", "JP", 126317000, 377973),
+    createData("France", "FR", 67022000, 640679),
+    createData("United Kingdom", "GB", 67545757, 242495),
+    createData("Russia", "RU", 146793744, 17098246),
+    createData("Nigeria", "NG", 200962417, 923768),
+    createData("Brazil", "BR", 210147125, 8515767),
+  ];
 
-        const vatSellPrice = Number((amount * sellPrice * vat).toFixed(2)); //ค่าธรรมเนียม 0.25% ขาย
-        const sales = Number((amount * sellPrice - vatSellPrice).toFixed(2)); //คำนวณ ยอดขาย
-        const profit = Number(sales - cost).toFixed(2); //คำนวณ กำไร
-        tempDate.push({
-          num: x + 1, //ไม้
-          buyPrice, //ชื้อราคา
-          cost, //ทุน
-          amount, //จำนวน
-          sellPrice, //ราคาขาย
-          sales, //ยอดขาย
-          profit, //กำไร
-        });
-        sum = sum + Number(profit);
-      }
-    }
+  const [useStyles, setUseStyles] = useState({
+    root: {
+      width: "100%",
+    },
+    container: {
+      maxHeight: 440,
+    },
+  });
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-    setSumProfit(sum.toLocaleString("en-US", { minimumFractionDigits: 2 }));
-    setRows(tempDate);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
   };
 
   return (
     <div>
-      <form noValidate autoComplete="off" style={{ paddingBottom: 25 }}>
-        <Grid container spacing={3}>
-          <Grid item md={4}></Grid>
-          <Grid item md={4}>
-            <TextField
-              id="standard-select-currency"
-              select
-              label="ประเภท"
-              value={modelSave.type}
-              onChange={(event) => {
-                setModelSave({ ...modelSave, type: event.target.value });
-              }}
-              fullWidth
-              style={{ paddingBottom: 5 }}
-            >
-              {currencies.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
-
-            <FormControl fullWidth style={{ paddingBottom: 7 }}>
-              <InputLabel htmlFor="standard-adornment-amount">ทุน</InputLabel>
-              <Input
-                id="standard-adornment-amount"
-                type="number"
-                value={modelSave.amount}
-                onChange={(event) => {
-                  setModelSave({ ...modelSave, amount: event.target.value });
-                }}
-                startAdornment={
-                  <InputAdornment position="start">฿</InputAdornment>
-                }
-              />
-            </FormControl>
-
-            <FormControl fullWidth style={{ paddingBottom: 7 }}>
-              <InputLabel htmlFor="standard-adornment-amount">ไม้</InputLabel>
-              <Input
-                id="standard-adornment-amount"
-                type="number"
-                value={modelSave.number}
-                onChange={(event) => {
-                  setModelSave({ ...modelSave, number: event.target.value });
-                }}
-                startAdornment={
-                  <InputAdornment position="start"> </InputAdornment>
-                }
-              />
-            </FormControl>
-
-            <Grid container spacing={3} style={{ paddingBottom: 10 }}>
-              <Grid item md={6}>
-                <FormControl fullWidth style={{ paddingBottom: 7 }}>
-                  <InputLabel htmlFor="standard-adornment-amount">
-                    เรทเริ่มต้น
-                  </InputLabel>
-                  <Input
-                    id="standard-adornment-amount"
-                    type="number"
-                    value={modelSave.rate_start}
-                    onChange={(event) => {
-                      setModelSave({
-                        ...modelSave,
-                        rate_start: event.target.value,
-                      });
-                    }}
-                    startAdornment={
-                      <InputAdornment position="start"> </InputAdornment>
-                    }
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item md={6}>
-                <FormControl fullWidth style={{ paddingBottom: 7 }}>
-                  <InputLabel htmlFor="standard-adornment-amount">
-                    ถึง
-                  </InputLabel>
-                  <Input
-                    id="standard-adornment-amount"
-                    type="number"
-                    value={modelSave.rate_end}
-                    onChange={(event) => {
-                      setModelSave({
-                        ...modelSave,
-                        rate_end: event.target.value,
-                      });
-                    }}
-                    startAdornment={
-                      <InputAdornment position="start"> </InputAdornment>
-                    }
-                  />
-                </FormControl>
-              </Grid>
-            </Grid>
-
-            <FormControl fullWidth style={{ paddingBottom: 7 }}>
-              <InputLabel htmlFor="standard-adornment-amount">
-                กำไร (%)
-              </InputLabel>
-              <Input
-                id="standard-adornment-amount"
-                type="number"
-                value={modelSave.profit}
-                onChange={(event) => {
-                  setModelSave({ ...modelSave, profit: event.target.value });
-                }}
-                startAdornment={
-                  <InputAdornment position="start"> </InputAdornment>
-                }
-              />
-            </FormControl>
-
-            <div style={{ textAlign: "center" }}>
-              <Button variant="contained" color="primary" onClick={calculate}>
-                คำนวณ
-              </Button>
-            </div>
-          </Grid>
-          <Grid item md={4}></Grid>
-        </Grid>
-      </form>
-
-      <h3 align="right">
-        {" "}
-        {rows.length > 0
-          ? `รวมกำไร (ประมาณ) : ${sumProfit.toLocaleString("en")} บาท`
-          : null}{" "}
-      </h3>
-      <TableContainer component={Paper}>
-        <Table size="small" aria-label="a dense table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">ไม้</TableCell>
-              <TableCell align="center">ชื้อราคา</TableCell>
-              <TableCell align="center">ทุน</TableCell>
-              <TableCell align="center">จำนวน</TableCell>
-              <TableCell align="center">ราคาขาย</TableCell>
-              <TableCell align="center">ยอดขาย (ประมาณ)</TableCell>
-              <TableCell align="center">กำไร (ประมาณ)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.length <= 0 ? (
-              <TableRow key="loading">
-                <TableCell align="center">
-                  <Skeleton />
-                </TableCell>
-                <TableCell align="center">
-                  <Skeleton />
-                </TableCell>
-                <TableCell align="center">
-                  <Skeleton />
-                </TableCell>
-                <TableCell align="center">
-                  <Skeleton />
-                </TableCell>
-                <TableCell align="center">
-                  <Skeleton />
-                </TableCell>
-                <TableCell align="center">
-                  <Skeleton />
-                </TableCell>
-                <TableCell align="center">
-                  <Skeleton />
-                </TableCell>
+      <Paper
+        style={{
+          width: "100%",
+        }}
+      >
+        <TableContainer
+          style={{
+            maxHeight: 700,
+          }}
+        >
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
               </TableRow>
-            ) : (
-              rows.map((row) => (
-                <TableRow key={row.num}>
-                  <TableCell align="center">{row.num}</TableCell>
-                  <TableCell align="center">{row.buyPrice}</TableCell>
-                  <TableCell align="center">{row.cost}</TableCell>
-                  <TableCell align="center">{row.amount}</TableCell>
-                  <TableCell align="center">{row.sellPrice}</TableCell>
-                  <TableCell align="center">{row.sales}</TableCell>
-                  <TableCell align="center">{row.profit}</TableCell>
-                </TableRow>
-              ))
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.code}
+                    >
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === "number"
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
     </div>
   );
 };
